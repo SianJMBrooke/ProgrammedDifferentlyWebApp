d18d56ca83a41be98915635b2c0c430dd648be95
SianJMBrooke <s.j.brooke@lse.ac.uk>
2023-11-28 16:12:25 +0100

    amend filepath

--- jcmcsite/settings.py
+++ jcmcsite/settings.py
@@ -126,10 +126,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'render/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'render/static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'render/static'), ]
 
 # Extra places for collectstatic to find static files.
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
