486c9c1bc16c822d334a4566193dbf5033466515
DrSiân <s.j.brooke@lse.ac.uk>
2023-11-28 15:47:08 +0100

    Update settings.py

--- jcmcsite/settings.py
+++ jcmcsite/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'render/static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # Extra places for collectstatic to find static files.
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
