2f436c9a6aa1d2be4f87424f9bea449b7d07a932
DrSiân <s.j.brooke@lse.ac.uk>
2023-11-28 14:58:10 +0100

    Update settings.py debug

--- jcmcsite/settings.py
+++ jcmcsite/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='b918f6aa5999c0b7ac7a7eddf0e40a46')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+# DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
