bc1e44a0ad11489bc26bdca655145850106d5a7b
SianJMBrooke <s.j.brooke@lse.ac.uk>
2023-12-04 20:36:51 +0100

    added unversioned files

--- render/forms.py
+++ render/forms.py
@@ -5,10 +5,8 @@ from .models import UploadModel
 class UploadForm(forms.ModelForm):
     class Meta:
         model = UploadModel
-        fields = ['name', 'email', 'github']
-    name = forms.CharField(max_length=100, label='Your full name')
-    email = forms.EmailField(label='Your e-mail address')
-    github = forms.URLField(label='Your GitHub profile URL')
+        fields = ['name', 'gender', 'email', 'github']
+
     file = forms.FileField(label='Upload your Python file', required=True,
                              widget=forms.FileInput(attrs={'accept': '.py'}))
     consent = forms.BooleanField(label='I consent to having my data stored and processed.')
--- render/models.py
+++ render/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 from django import forms
 
+GENDER_CHOICES = (
+    ("SELECT", "Please Choose"),
+    ("WOMAN", "Woman"),
+    ("MAN", "Man"),
+    ("NONBINARY", "Non-binary"),
+    ("FLUID", "Gender Fluid"),
+    ("TRANS", "Transgender"),
+    ("NOTSAY", "Prefer not to say")
+)
+
 
 class UploadModel(models.Model):
     name = models.CharField(max_length=100, verbose_name='Your full name')
+    gender = models.CharField(max_length=9, choices=GENDER_CHOICES, default='SELECT')
     email = models.EmailField(verbose_name='Your e-mail address')
     github = models.URLField(verbose_name='Your GitHub profile URL')
     file = forms.FileField(label='Upload your Python file', required=True,
