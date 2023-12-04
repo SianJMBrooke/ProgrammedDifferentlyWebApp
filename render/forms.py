1cbdd9d7f6aeef45bb16525db74b24a7379d7365
SianJMBrooke <s.j.brooke@lse.ac.uk>
2023-12-04 18:01:22 +0100

    Revert "adding to db"
    This reverts commit 86c09891e06b8b6be399e6e2d32e570da159edad.

--- render/forms.py
+++ render/forms.py
@@ -6,7 +6,13 @@ class UploadForm(forms.ModelForm):
     class Meta:
         model = UploadModel
         fields = ['name', 'email', 'github']
-
+    name = forms.CharField(max_length=100, label='Your full name')
+    email = forms.EmailField(label='Your e-mail address')
+    github = forms.URLField(label='Your GitHub profile URL')
     file = forms.FileField(label='Upload your Python file', required=True,
-                           widget=forms.FileInput(attrs={'accept': '.py'}))
+                             widget=forms.FileInput(attrs={'accept': '.py'}))
     consent = forms.BooleanField(label='I consent to having my data stored and processed.')
+
+
+
+
